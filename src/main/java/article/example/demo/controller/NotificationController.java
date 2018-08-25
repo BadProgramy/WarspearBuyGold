c95c3d4d52c73e4010cac6183b1d84e160eaf1aa
BadProgramy <airatmyhytdinov08@gmail.com>
2018-08-25 18:17:42 +0300

    Добавил уведомления

--- src/main/java/article/example/demo/controller/NotificationController.java
+++ src/main/java/article/example/demo/controller/NotificationController.java
@@ -1,6 +1,7 @@
 package article.example.demo.controller;
 
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,22 @@ import java.util.Map;
 @RequestMapping("/warspear")
 public class NotificationController {
 
+    @Autowired
+    private Sender sender;
+
     @RequestMapping(value = "/notification", method = RequestMethod.POST)
-    public String notification(String notification_type, String operation_id, String label, String datetime,
+    public void notification(String notification_type, String operation_id, String label, String datetime,
                                             Float amount, Float withdraw_amount, String sender, String sha1_hash, String currency, Boolean codepro) {
-        return operation_id;
+        this.sender.send("Получилось",
+                notification_type + " \n"+
+                        operation_id + " \n" +
+        label + " \n" +
+        datetime + " \n" +
+        String.valueOf(amount) + " \n" +
+        String.valueOf(withdraw_amount) + " \n" +
+        sender + " \n" +
+        sha1_hash + " \n" +
+        currency + " \n" +
+        codepro, "myhytdinov@yandex.ru");
     }
 }
