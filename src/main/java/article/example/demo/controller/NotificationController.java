30805b700659b6da5d832a4364c7bc9d0e638109
BadProgramy <airatmyhytdinov08@gmail.com>
2018-08-25 22:52:21 +0300

    Добавляю notification

--- src/main/java/article/example/demo/controller/NotificationController.java
+++ src/main/java/article/example/demo/controller/NotificationController.java
@@ -33,8 +33,8 @@ public class NotificationController {
                         operation_id + " " +
         label + " " +
         datetime + " " +
-        /*String.valueOf(amount) + " " +
-        String.valueOf(withdraw_amount) + " " +*/
+        String.valueOf(amount) + " " +
+        String.valueOf(withdraw_amount) + " " +
         sender + " " +
         sha1_hash + " " +
         currency + " " +
