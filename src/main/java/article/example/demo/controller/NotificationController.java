4ea2657c21bf9c64490028f7d2d91f96cc08df8b
BadProgramy <airatmyhytdinov08@gmail.com>
2018-08-27 20:23:23 +0300

    добавил странички для покупки анонимно у ух аккаунты

--- src/main/java/article/example/demo/controller/NotificationController.java
+++ src/main/java/article/example/demo/controller/NotificationController.java
@@ -51,7 +51,7 @@ public class NotificationController {
 
         try {
             if (paramStringHash1.equals(sha1_hash) && !codepro && !unaccepted) {
-                AccountWithGold accountWithGold = accountWithGoldService.findOne(amount.doubleValue(), label);
+                AccountWithGold accountWithGold = accountWithGoldService.findOne(withdraw_amount.doubleValue(), label);
                 this.sender.send("Ваш аккаунт",
                         "Вот аккаунт на котором лежит ваше золото:" +
                                 " login - " + accountWithGold.getLogin() +
