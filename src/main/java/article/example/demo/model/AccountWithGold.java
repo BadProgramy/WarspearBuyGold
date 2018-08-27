b35ba409639904c2a1cc6dd1448fac974f9f2bed
BadProgramy <airatmyhytdinov08@gmail.com>
2018-08-27 17:30:39 +0300

    добавил странички для покупки анонимно у ух аккаунты

--- src/main/java/article/example/demo/model/AccountWithGold.java
+++ src/main/java/article/example/demo/model/AccountWithGold.java
@@ -4,9 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
-public class AccountWithGold {
+public class AccountWithGold implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
